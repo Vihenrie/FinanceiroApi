5a35bda1a5ada46a42528db3c513d252d76eb10e
Vihenrie <viniciushpintolp@gmail.com>
2023-06-23 12:24:09 -0300

    att

---
+++ src/main/java/br/com/etec/vinicius/FinanceiroApi/repository/filter/ClienteFilter.java
@@ -0,0 +1,14 @@
+package br.com.etec.vinicius.FinanceiroApi.repository.filter;
+
+public class ClienteFilter {
+
+    private String NomeCliente;
+
+    public String getNomeCliente() {
+        return NomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        NomeCliente = nomeCliente;
+    }
+}
---
+++ src/main/java/br/com/etec/vinicius/FinanceiroApi/repository/filter/ContasReceberFilter.java
@@ -0,0 +1,11 @@
+package br.com.etec.vinicius.FinanceiroApi.repository.filter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class ContasReceberFilter {
+
+    private LocalDate DataConta;
+    private BigDecimal ValorConta;
+
+}
